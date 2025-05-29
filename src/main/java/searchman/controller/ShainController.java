ee77f42fcf9e8978ee500159a8dc77f7f4433027
田中　琉聖 <ryusei_tanaka@a7dsc215.local>
2025-05-29 11:51:30 +0900

    raku

--- src/main/java/searchman/controller/ShainController.java
+++ src/main/java/searchman/controller/ShainController.java
@@ -8,21 +8,41 @@ public class ShainController {
 
 	@GetMapping("/test")
 	public String test() {
-		return "/WEB-INF/test.jsp";
+		return "test";
 	}
 
 	@GetMapping("/insert")
 	public String insert() {
-		return "/WEB-INF/insert.jsp";
+		return "insert";
 	}
 
 	@GetMapping("/index")
 	public String index() {
-		return "/WEB-INF/index.jsp";
+		return "index";
 	}
 
 	@GetMapping("/update")
 	public String update() {
-		return "/WEB-INF/update.jsp";
+		return "update";
 	}
+	
+//	@GetMapping("/test")
+//	public String test() {
+//		return "/WEB-INF/test.jsp";
+//	}
+//
+//	@GetMapping("/insert")
+//	public String insert() {
+//		return "/WEB-INF/insert.jsp";
+//	}
+//
+//	@GetMapping("/index")
+//	public String index() {
+//		return "/WEB-INF/index.jsp";
+//	}
+//
+//	@GetMapping("/update")
+//	public String update() {
+//		return "/WEB-INF/update.jsp";
+//	}
 }
