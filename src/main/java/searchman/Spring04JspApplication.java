43432145cb1715f07221f3fed49deb2d7c4f5faf
田中　琉聖 <ryusei_tanaka@a7dsc215.local>
2025-05-29 10:27:38 +0900

    moving static html file

---
+++ src/main/java/searchman/Spring04JspApplication.java
@@ -0,0 +1,13 @@
+package searchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring04JspApplication.class, args);
+	}
+
+}
